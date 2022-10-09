f20b66ed407aa236b4b7a82c3c530fb07acf6feb
tooffuu <thdud8866@naver.com>
2022-10-09 23:39:20 +0900

    Home style design

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../Style/Home/Home.scss";
+
+const Home = () => {
+  return (
+    <>
+      <div className="background">
+        <div className="home">
+          <div className="home_logo">
+            <img src="/images/placeholder.png" />
+            <a href="/">넌 어때?</a>
+            <input
+              type="text"
+              placeholder="니가 알고싶은 리뷰, 여기 다 있어!"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
