47d4468f0825c34856bd762af9c7a138c18cd090
tooffuu <thdud8866@naver.com>
2022-10-14 15:02:47 +0900

    회원가입 api 연동

--- src/Components/Sign/SignUp.jsx
+++ src/Components/Sign/SignUp.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { BACKEND_URL } from "../../utils";
 import Topbar from "../Main/Topbar";
 import "../../Style/Sign/SignUp.scss";
 
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [userid, setUserid] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <>
       <Topbar />
-      <body className="signUpBody">
+      <div className="signUpBody">
         <div className="signUpBack">
           <div className="signUptemplate">
             <div className="signUpLogo">
@@ -14,36 +23,102 @@ const SignUp = () => {
               <hr />
             </div>
             <div className="signUpForm">
-              <form>
+              <form
+                onSubmit={async (e) => {
+                  e.preventDefault();
+                  try {
+                    const data = await axios({
+                      url: "http://localhost:8085/api/v1/user/join",
+                      method: "POST",
+                      data: {
+                        username,
+                        nickname,
+                        userid,
+                        password1,
+                        password2,
+                        email,
+                      },
+                    });
+                    console.log(data);
+                  } catch (e) {
+                    console.log({
+                      username,
+                      nickname,
+                      userid,
+                      password1,
+                      password2,
+                      email,
+                    });
+                  }
+                }}
+              >
                 <div>
                   이름
                   <br />
-                  <input type="text" placeholder="이름을 입력해주세요" />
+                  <input
+                    type="text"
+                    placeholder="이름을 입력해주세요"
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                  />
                 </div>
                 <div>
                   별명
                   <br />
-                  <input type="text" placeholder="사용할 별명을 입력해주세요" />
+                  <input
+                    type="text"
+                    placeholder="사용할 별명을 입력해주세요"
+                    value={nickname}
+                    onChange={(e) => {
+                      setNickname(e.target.value);
+                    }}
+                  />
                 </div>
                 <div>
                   ID
                   <br />
-                  <input type="text" />
+                  <input
+                    type="text"
+                    value={userid}
+                    onChange={(e) => {
+                      setUserid(e.target.value);
+                    }}
+                  />
                 </div>
                 <div>
                   password
                   <br />
-                  <input type="text" />
+                  <input
+                    type="password"
+                    value={password1}
+                    onChange={(e) => {
+                      setPassword1(e.target.value);
+                    }}
+                  />
                 </div>
                 <div>
                   Confirm password
                   <br />
-                  <input type="text" />
+                  <input
+                    type="password"
+                    value={password2}
+                    onChange={(e) => {
+                      setPassword2(e.target.value);
+                    }}
+                  />
                 </div>
                 <div>
                   E-mail
                   <br />
-                  <input type="text" />
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
                 </div>
                 <button type="submit" className="signUpButton">
                   가입하기
@@ -55,7 +130,7 @@ const SignUp = () => {
             </div>
           </div>
         </div>
-      </body>
+      </div>
     </>
   );
 };
