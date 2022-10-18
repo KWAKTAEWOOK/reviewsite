67eb3ed68267fe162dba3e4515dfbb50f6d92e25
tooffuu <thdud8866@naver.com>
2022-10-18 18:31:18 +0900

    로그인

--- src/Components/Sign/Login.jsx
+++ src/Components/Sign/Login.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Topbar from "../Main/Topbar";
 import "../../Style/Sign/Login.scss";
+import axios from "axios";
+import { BACKEND_URL } from "../../utils";
 
 const Login = () => {
+  const [userid, setUserid] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
       <Topbar />
@@ -14,16 +18,50 @@ const Login = () => {
               <hr />
             </div>
             <div className="signUpForm">
-              <form>
+              <form
+                onSubmit={async (e) => {
+                  e.preventDefault();
+                  try {
+                    const data = await axios({
+                      url: `${BACKEND_URL}/user/login`,
+                      method: "POST",
+                      data: {
+                        userid,
+                        password,
+                      },
+                    });
+                    setUserid("");
+                    setPassword("");
+                    console.log(data);
+                    alert("로그인 성공!");
+                  } catch (e) {
+                    // e.text().then((msg) => alert(msg));
+                    console.log(e);
+                    alert("로그인 실패!");
+                  }
+                }}
+              >
                 <div>
                   ID
                   <br />
-                  <input />
+                  <input
+                    type="text"
+                    value={userid}
+                    onChange={(e) => {
+                      setUserid(e.target.value);
+                    }}
+                  />
                 </div>
                 <div>
                   password
                   <br />
-                  <input />
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
                 </div>
                 <button type="submit" className="signUpButton">
                   로그인
