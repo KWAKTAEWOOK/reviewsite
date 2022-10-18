cee89020852e507196e25e656b9414f2c1e5d79b
junhp9355 <junhp9355@naver.com>
2022-10-18 17:53:40 +0900

    Main 페이지 작업중

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,19 +3,7 @@ import "../../Style/Home/Home.scss";
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <body className="HomeBody">
-      <div className="background">
-        <div className="home">
-          <div className="home_logo">
-            <img src="/images/placeholder.png" alt="0" />
-            <a href="/main">넌 어때?</a>
-            <input
-              type="text"
-              placeholder="니가 알고싶은 리뷰, 여기 다 있어!"
-            />
-=======
-    <>
+    <body>
       <div className="home_body">
         <div className="background">
           <div className="home">
@@ -27,7 +15,6 @@ const Home = () => {
                 placeholder="니가 알고싶은 리뷰, 여기 다 있어!"
               />
             </div>
->>>>>>> master
           </div>
         </div>
       </div>
