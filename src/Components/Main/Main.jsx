77a51c6bae182b1306fbf59fadfe95994a63c145
junhp9355 <junhp9355@naver.com>
2022-10-21 15:52:29 +0900

    Main 카테고리 수정

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,6 +4,7 @@ import LeftArrow from "../../Style/image/left-arrow.png";
 import Topbar from "./Topbar";
 import "../../Style/Main/Main.scss";
 import useGeolocation from "react-hook-geolocation";
+import { Link } from "react-router-dom";
 
 const { kakao } = window;
 
@@ -16,13 +17,35 @@ const Main = ({
   searchSubmit,
 }) => {
   const [Places, setPlaces] = useState([]);
+  const [backupAll, setBackupAll] = useState([]);
+  const [backupFD6, setBackupFD6] = useState([]);
+  const [backupCE7, setBackupCE7] = useState([]);
+  const [backupCS2, setBackupCS2] = useState([]);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
   const [open, setOpen] = useState(true);
   const [arrow, setArrow] = useState(true);
-  const [dropdownVisibility, setDropdownVisibility] = useState(false);
 
-  const CategorySelect = (e) => {};
+  const onChangeCategory = (e) => {
+    if (e.target.value === "FD6") {
+      var filterFD6 = backupFD6.filter(
+        (value) => value.category_group_code === "FD6"
+      );
+      setPlaces(filterFD6);
+    } else if (e.target.value === "all") {
+      setPlaces(backupAll);
+    } else if (e.target.value === "CE7") {
+      var filterCE7 = backupCE7.filter(
+        (value) => value.category_group_code === "CE7"
+      );
+      setPlaces(filterCE7);
+    } else if (e.target.value === "CS2") {
+      var filterCS2 = backupCS2.filter(
+        (value) => value.category_group_code === "CS2"
+      );
+      setPlaces(filterCS2);
+    }
+  };
 
   const toggleSearchList = () => {
     setOpen((open) => !open);
@@ -37,24 +60,22 @@ const Main = ({
   const offset = (page - 1) * limit;
 
   useEffect(() => {
-    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-    var markers = [];
     var mapContainer = document.getElementById("map");
 
     var mapOption = {
-      center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
-      level: 2, // 지도의 확대 레벨
+      center: new kakao.maps.LatLng(lat, lng),
+      level: 2,
     };
 
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
     const ps = new kakao.maps.services.Places();
-    ps.keywordSearch(place, placesSearchCB);
+    ps.keywordSearch(place, placesSearchCB, { useMapBounds: true });
 
     var geocoder = new kakao.maps.services.Geocoder();
 
-    var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
-      infowindow = new kakao.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+    var marker = new kakao.maps.Marker(),
+      infowindow = new kakao.maps.InfoWindow({ zindex: 1 });
     searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
     kakao.maps.event.addListener(map, "click", function (mouseEvent) {
@@ -74,39 +95,32 @@ const Main = ({
             detailAddr +
             "</div>";
 
-          // 마커를 클릭한 위치에 표시합니다
           marker.setPosition(mouseEvent.latLng);
           marker.setMap(map);
 
-          // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
           infowindow.setContent(content);
           infowindow.open(map, marker);
         }
       });
     });
 
-    // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
     kakao.maps.event.addListener(map, "idle", function () {
       searchAddrFromCoords(map.getCenter(), displayCenterInfo);
     });
 
     function searchAddrFromCoords(coords, callback) {
-      // 좌표로 행정동 주소 정보를 요청합니다
       geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
     }
 
     function searchDetailAddrFromCoords(coords, callback) {
-      // 좌표로 법정동 상세 주소 정보를 요청합니다
       geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
     }
 
-    // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
     function displayCenterInfo(result, status) {
       if (status === kakao.maps.services.Status.OK) {
         var infoDiv = document.getElementById("centerAddr");
 
         for (var i = 0; i < result.length; i++) {
-          // 행정동의 region_type 값은 'H' 이므로
           if (result[i].region_type === "H") {
             infoDiv.innerHTML = result[i].address_name;
             break;
@@ -127,6 +141,10 @@ const Main = ({
         map.setBounds(bounds);
         displayPagination(pagination);
         setPlaces(data);
+        setBackupAll(data);
+        setBackupCE7(data);
+        setBackupCS2(data);
+        setBackupFD6(data);
         console.log(data);
       }
     }
@@ -169,7 +187,7 @@ const Main = ({
       kakao.maps.event.addListener(marker, "click", function () {
         infowindow.setContent(
           '<div style="padding:13px;font-size:12px; color:black; width:300px; height:240px; overflow:hidden">' +
-            `<a href="https://place.map.kakao.com/${place.id}#comment" style="display:block; margin-top:-2px; font-size:16px; font-weight:bold">` +
+            `<a href="http://localhost:3000/Detail" style="display:block; margin-top:-2px; font-size:16px; font-weight:bold">` +
             place.place_name +
             "</a>" +
             '<img src="https://ldb-phinf.pstatic.net/20211228_153/1640684993923s9vPb_JPEG/KakaoTalk_20211228_093917446_03.jpg" alt="0" style="width:300px; height:150px; margin-top:7px; display:block"/>' +
@@ -201,17 +219,23 @@ const Main = ({
               >
                 <img
                   className="ShowAndHideBt"
-                  src={arrow ? RightArrow : LeftArrow}
+                  src={arrow ? LeftArrow : RightArrow}
                   alt="slidebt"
                 />
               </button>
               <div className="searchMenu">
                 <div className="searchBtn">
                   <form className="searchBtn_in" onSubmit={searchSubmit}>
-                    <select name="" className="category" id="">
-                      <option value="">전체</option>
-                      <option value="">음식점</option>
-                      <option value="">카페</option>
+                    <select
+                      name="category"
+                      className="category"
+                      defaultValue="all"
+                      onChange={onChangeCategory}
+                    >
+                      <option value="all">전체</option>
+                      <option value="FD6">음식점</option>
+                      <option value="CE7">카페</option>
+                      <option value="CS2">편의점</option>
                     </select>
                     <input
                       className="searchInput"
@@ -227,35 +251,41 @@ const Main = ({
                 <div className="search_List">
                   <div className="search_List_fixed">
                     {Places.slice(offset, offset + limit).map((item, i) => (
-                      <ul className="menulist" key={i}>
-                        <li className="searchInfoList">
-                          <a href={item.place_url} className="searchInfoName">
-                            {item.place_name}
-                          </a>
-                          <li className="searchInfoCategory">
-                            {item.category_name}
+                      <Link
+                        to={{
+                          pathname: "/Detail",
+                          state: {
+                            place_name: item.place_name,
+                            CategoryName: item.category_name,
+                            Address: item.road_address_name,
+                            posX: item.x,
+                            posY: item.y,
+                          },
+                        }}
+                      >
+                        <ul className="menulist" key={i}>
+                          <li className="searchInfoList">
+                            <a href={item.place_url} className="searchInfoName">
+                              {item.place_name}
+                            </a>
+                            <li className="searchInfoCategory">
+                              {item.category_name}
+                            </li>
+                            <div className="menuSublist">
+                              {item.road_address_name ? (
+                                <div className="searchInfoAddress">
+                                  <span>{item.road_address_name}</span>
+                                </div>
+                              ) : (
+                                <span className="searchInfoAddress">
+                                  {item.address_name}
+                                </span>
+                              )}
+                              <li className="searchInfoPhone">{item.phone}</li>
+                            </div>
                           </li>
-                          <div className="searchInfoImg">
-                            <img
-                              src="https://ldb-phinf.pstatic.net/20211228_153/1640684993923s9vPb_JPEG/KakaoTalk_20211228_093917446_03.jpg"
-                              alt="0"
-                            />
-                          </div>
-
-                          <div className="menuSublist">
-                            {item.road_address_name ? (
-                              <div className="searchInfoAddress">
-                                <span>{item.road_address_name}</span>
-                              </div>
-                            ) : (
-                              <span className="searchInfoAddress">
-                                {item.address_name}
-                              </span>
-                            )}
-                            <li className="searchInfoPhone">{item.phone}</li>
-                          </div>
-                        </li>
-                      </ul>
+                        </ul>
+                      </Link>
                     ))}
                   </div>
                 </div>
