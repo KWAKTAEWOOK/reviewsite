cd0d62fdd2809f24f6187e8b6131c00d74aa8c2a
tooffuu <thdud8866@naver.com>
2022-10-28 02:13:50 +0900

    탈퇴 기능추가

---
+++ src/Components/Sign/DeleteUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Style/Sign/DeleteUser.scss";
+
+const DeleteUser = () => {
+  return (
+    <>
+      <div className="modalBack"></div>
+    </>
+  );
+};
+
+export default DeleteUser;
