9b3c0761b301479351b777519f0abcb822574322
FullSoYu <hey6410@naver.com>
2022-11-28 18:38:19 +0900

    가나 한국과 가나 16강 가나

--- src/Components/Mypage/MypageTest.jsx
+++ src/Components/Mypage/MypageTest.jsx
@@ -41,22 +41,37 @@ const MypageTest = () => {
       <div className="MypageEditBoxContainer">
         <div className="MypageEditBox">
           {/* <FontAwesomeIcon icon={faUserSecret} className="Usericon" /> */}
+          <div className="MypageEdit_userIdbox">
+            <div className="MypageEdit_userIdbox_subject MyPageEdit_box_subject">
+              ID
+            </div>
+            <div className="MypageEdit_userIdbox_ID MyPageEdit_box_content">{`${userid}`}</div>
+          </div>
           <div className="MypageEdit_usernamebox">
             <div className="MypageEdit_usernamebox_subject MyPageEdit_box_subject">
               이름
             </div>
-            <div className="MypageEdit_usernamebox_username">{`${username}`}</div>
+            <div className="MypageEdit_usernamebox_username MyPageEdit_box_content">{`${username}`}</div>
           </div>
           <div className="MypageEdit_nicknamebox">
             <div className="MypageEdit_nicknamebox_subject MyPageEdit_box_subject">
               별명
             </div>
-            <div className="MypageEdit_nicknamebox_nickname">
+            <div className="MypageEdit_nicknamebox_nickname MyPageEdit_box_content">
               <input
                 type="text"
                 onChange={changeNickname}
                 value={nickname}
               ></input>
+            </div>
+          </div>
+          <div className="MypageEdit_passwordbox">
+            비밀번호 변경
+            <div className="MypageEdit_passwordbox_newpasswordbox_subject MypageEdit_box_subject">
+              새 비밀번호입력
+            </div>
+            <div className="MypageEdit_passwordbox_newpasswordbox_content MyPageEdit_box_content">
+              <input type="text" onChange={changeNickname} value={""}></input>
             </div>
           </div>
         </div>
