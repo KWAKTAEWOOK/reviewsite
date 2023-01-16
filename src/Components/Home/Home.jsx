9f353551dcb575327bb756d2b6bbe884bbec7e53
junhp9355 <junhp9355@naver.com>
2023-01-16 17:10:12 +0900

    일부 오류코드 수정

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,51 +6,20 @@ const Home = ({ onChange, inputText, setKeywords }) => {
     e.preventDefault();
     window.location.href = "/main";
   };
-  const onClickHomeLogo = () => {
-    sessionStorage.removeItem("search");
-    sessionStorage.removeItem("keywords");
-  };
 
   const searchIcon = "/images/search.png";
   const mainPicture = "/images/The Munchies Family Style.png";
   return (
     <>
-      {/* <div className="home_body">
-        <div className="background">
-          <div className="home">
-            <div className="home_logo">
-              <img src="/images/placeholder.png" alt="logo" />
-              <a href="/main" onClick={onClickHomeLogo}>
-                넌 어때?
-              </a>
-              <form onSubmit={homeSearchSubmit}>
-                <input
-                  className="HomeInput"
-                  type="text"
-                  value={inputText}
-                  placeholder="니가 알고싶은 리뷰, 여기 다 있어!"
-                  onChange={onChange}
-                />
-                <button className="HomeSearchbt" type="submit"></button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div className="home_body">
-        {/* <div className="background"> */}
         <div className="home">
-          {/* <img src="/images/placeholder.png" alt="logo" />
-              <a href="/main" onClick={onClickHomeLogo}>
-                넌 어때?
-              </a> */}
-          <p className="home_main_p">
+          <div className="home_main_p">
             니가 알고싶은 <span className="home_main_span">리뷰</span>, 여기 다
             있어!
             <p className="home_logo_p">
               넌 어때 <span className="home_logo_span">?</span>
             </p>
-          </p>
+          </div>
 
           <form onSubmit={homeSearchSubmit}>
             <input
@@ -61,12 +30,11 @@ const Home = ({ onChange, inputText, setKeywords }) => {
               onChange={onChange}
             />
             <button className="HomeSearchbt" type="submit">
-              <img className="HomeSearchbt" src={searchIcon} />
+              <img className="HomeSearchbt" src={searchIcon} alt="bt" />
             </button>
           </form>
-          <img className="main_image" src={mainPicture} />
+          <img className="main_image" src={mainPicture} alt="main" />
         </div>
-        {/* </div> */}
       </div>
     </>
   );
